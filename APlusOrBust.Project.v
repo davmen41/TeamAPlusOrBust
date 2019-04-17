e8a7e87fa506e461f035d7170deede51239d3da6
David Mendoza <dmdmendoza719@gmail.com>
2019-04-16 21:49:15 -0500

    Fixes error in state

--- APlusOrBust.Project.v
+++ APlusOrBust.Project.v
@@ -14,36 +14,38 @@ module AddFull (input a, b, carry_in, output carry_out, sum);
 	AddHalf M2 (w2, carry_in, w3, sum);
 	or (carry_out, w1, w3);
 endmodule
- /*
-  *	State 2: Load ship
-  */
-
 module comparator(input [3:0] A, B, output result);
 	wire a1,a2,a3,a4; // AND outputs
 	wire [7:0] andOutput;
 	wire [3:0] xnorOutput;
-	wire aLessThanB; wire result;wire equalsOutput;
+	wire aLessThanB; 
+	wire result;
+	wire equalsOutput;
 	
-	int j = 0;
-	for(i=0; i<7; i=i+2; j=j+1)begin
-		and(andOutput[i],~a[j],b[j]);
-		and(andOutput[i+1],a[j],~b[j]);
-		xnor(xnorOutput[j], andOutput[i], andOutput[i+1]);
-	end
+	assign andOutput[0] = ~A[3] & B[3];
+	assign andOutput[1] = A[3] & ~B[3];
+	assign andOutput[2] = ~A[2] & B[2];
+	assign andOutput[3] = A[2] & ~B[2];
+	assign andOutput[4] = ~A[1] & B[1];
+	assign andOutput[5] = A[1] & ~B[1];
+	assign andOutput[6] = ~A[0] & B[0];
+	assign andOutput[7] = A[0] & ~B[0];
 	
-	and(a1,~andOutput[2], xnorOutput[0]);
+	xnor(xnorOutput[0], andOutput[0],andOutput[1]);
+	xnor(xnorOutput[1], andOutput[2],andOutput[3]);
+	xnor(xnorOutput[2], andOutput[4],andOutput[5]);
+	xnor(xnorOutput[3], andOutput[6],andOutput[7]);
+	
+	assign a1 = andOutput[2] & xnorOutput[0];
 	assign a2 = xnorOutput[0] & xnorOutput[1] & andOutput[4];
 	assign a3 = xnorOutput[0] & xnorOutput[1] & xnorOutput[2] & andOutput[6]; 
-	assign aLessThanB = a1 | a2 | a3 | andOutput[0]
-	
-	for(i=0; i< 4; i=i+1)begin
-		assign equalsOutput &= xnorOutput[i]; 
-	end
+	assign aLessThanB = a1 | a2 | a3 | andOutput[0];
+	assign equalsOutput = xnorOutput[0] & xnorOutput[1] & xnorOutput[2] & xnorOutput[3];
 	
 	assign result = equalsOutput | aLessThanB;
 endmodule
 
-module equals(input [3:0] A, B, output result);
+module equals(input [3:0] a, b, output result);
 	wire x0,x1,x2,x3, r1, r2;
 	
 	xnor G1(x0, a[0], b[0]);
@@ -55,20 +57,46 @@ module equals(input [3:0] A, B, output result);
 	and G5(r2,x2,x3);
 	and ResultGate(result,r1,r2);
 endmodule  
+/*
+ * State 1: Rest State
+ */
+ module rest(output nextState);
+ 
+	//do nothing for now
+	assign nextState = 3'b001;
+ endmodule
+ 
+ /*
+  *	State 2: Load ship
+  */
 
 module LoadShip (input [3:0] crewSize, passengerSize, cargoSpace, output nextState);
+	wire [3:0] fixedPassengerSize; wire [3:0] fixedCrewSize; wire [3:0] fixedCargoSize;
+	wire isShipFull; wire isCrewOnBoard; wire isCargoFull;
+	reg [2:0] state; wire nextState;
+	
+	assign fixedPassengerSize = 4'b1111;
+	assign fixedCrewSize = 4'b0100;
+	assign fixedCargoSize = 4'b1111;
+	
 	//check passenger size
-	comparator(passengerSize,4'b1111,isShipFull);
+	comparator  checkPassengerSize(passengerSize,fixedPassengerSize,isShipFull);
 	
 	//check crew size
-	equals(crewSize,4'b0100,isCrewOnBoard);
+	equals checkCrewSize(crewSize,fixedCrewSize,isCrewOnBoard);
 
 	//check cargo space size
-	comparator(cargoSpace,4'b1111,isCargoFull);
+	comparator checkCargoSize(cargoSpace,fixedCargoSize,isCargoFull);
 	
-	if(isShipFull & isCrewOnBoard & isCargoFull)begin
-		assign nextState = 3'010
-	end else begin
-		assign nextState = 3'b111
+	wire shipLoaded = isShipFull & isCrewOnBoard & isCargoFull;
+
+	always @* begin
+		if(shipLoaded)begin
+			state <= 3'b010;
+		end else begin
+			state <= 3'b111;
+		end
 	end
+	
+	assign nextState = state;
 endmodule 
